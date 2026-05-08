c7b8541d233ddb53a870408a0a18787dc813e2c8
Arpit Chhabra <156332722+ArpitChb2704@users.noreply.github.com>
2026-05-08 23:58:33 +0530

    Change embedding model in ranking service

--- app/services/ranking.py
+++ app/services/ranking.py
@@ -2,7 +2,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
+    model_name="BAAI/bge-small-en-v1.5" 
 )
 
 vectorstore = FAISS.load_local(
